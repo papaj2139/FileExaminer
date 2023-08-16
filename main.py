5e84bf5d680756887edb2262cecb69bc04444122
papaj2139 <95711852+papaj2139@users.noreply.github.com>
2023-08-16 22:31:19 +0200

    VERSION 1.2

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ def get_size_recursive(path, max_depth=None, include_hidden=False, min_file_size
             if valid_extensions and not any(filename.lower().endswith(ext) for ext in valid_extensions):
                 continue
             total_size += file_size
-            print(filepath)
     return total_size
 
 def format_size(size_bytes):
@@ -37,7 +36,7 @@ def is_important_system_file(file_path):
     important_extensions = ['.sys', '.dll', '.exe']
     return any(file_path.lower().endswith(ext) for ext in important_extensions)
 
-def process_directory(queue, directory, max_depth, include_hidden, min_file_size, valid_extensions, resource_level):
+def process_directory(queue, directory, max_depth, include_hidden, min_file_size, valid_extensions, resource_level, pause_event):
     total_size = 0
     for dirpath, dirnames, filenames in os.walk(directory, topdown=True):
         if not include_hidden:
@@ -56,21 +55,34 @@ def process_directory(queue, directory, max_depth, include_hidden, min_file_size
             total_size += file_size
             queue.put((filepath, file_size))
         
-        # Monitor resource usage based on the resource_level option
         if resource_level == "low":
-            time.sleep(0.1)  # Simulate low resource usage
+            time.sleep(0.1)
         elif resource_level == "medium":
             process = psutil.Process(os.getpid())
             process.cpu_percent(interval=0.1)
-            time.sleep(0.05)  # Simulate moderate resource usage
+            process.memory_percent()
+            time.sleep(0.05)
         elif resource_level == "high":
             process = psutil.Process(os.getpid())
             process.cpu_percent(interval=0.01)
-            time.sleep(0.01)  # Simulate high resource usage
+            process.memory_percent()
+            time.sleep(0.01)
+        if pause_event.is_set():
+            print("Scanning paused. Press 'r' to resume or 'q' to quit.")
+            while True:
+                choice = input()
+                if choice == 'r':
+                    pause_event.clear()
+                    break
+                elif choice == 'q':
+                    queue.put(None)
+                    return
+                else:
+                    print("Invalid choice. Press 'r' to resume or 'q' to quit.")
     queue.put(None)
 
 def main_threaded(scan_type, directory_to_scan, max_depth, include_hidden, min_file_size, valid_extensions,
-                  num_files_to_display, output_file, resource_level):
+                  num_files_to_display, output_file, resource_level, history_logging, pause_event):
     files = []
     folders = []
     num_files_scanned = 0
@@ -79,10 +91,23 @@ def main_threaded(scan_type, directory_to_scan, max_depth, include_hidden, min_f
     queue = Queue()
 
     scanner_thread = threading.Thread(target=process_directory, args=(queue, directory_to_scan, max_depth,
-                                                                      include_hidden, min_file_size, valid_extensions, resource_level))
+                                                                      include_hidden, min_file_size, valid_extensions, resource_level, pause_event))
     scanner_thread.start()
 
     while True:
+        if pause_event.is_set():
+            print("Scanning paused. Press 'r' to resume or 'q' to quit.")
+            while True:
+                choice = input()
+                if choice == 'r':
+                    pause_event.clear()
+                    break
+                elif choice == 'q':
+                    scanner_thread.join()
+                    return
+                else:
+                    print("Invalid choice. Press 'r' to resume or 'q' to quit.")
+
         result = queue.get()
         if result is None:
             break
@@ -97,14 +122,13 @@ def main_threaded(scan_type, directory_to_scan, max_depth, include_hidden, min_f
         time_per_file = elapsed_time / max(1, num_files_scanned)
         estimated_remaining_time = (total_files - num_files_scanned) * time_per_file
 
-        # Print progress to console
         print(f"Progress: {num_files_scanned}/{total_files} files scanned.")
         print(f"Estimated Time Remaining: {estimated_remaining_time:.2f} seconds")
 
-        # Append progress to history.log
-        with open('history.log', 'a') as history_file:
-            history_file.write(f"Progress: {num_files_scanned}/{total_files} files scanned.\n")
-            history_file.write(f"Estimated Time Remaining: {estimated_remaining_time:.2f} seconds\n")
+        if history_logging:
+            with open('history.log', 'a') as history_file:
+                history_file.write(f"Progress: {num_files_scanned}/{total_files} files scanned.\n")
+                history_file.write(f"Estimated Time Remaining: {estimated_remaining_time:.2f} seconds\n")
 
     files.sort(key=lambda x: x[1], reverse=True)
     folders.sort(key=lambda x: x[1], reverse=True)
@@ -127,30 +151,29 @@ def main_threaded(scan_type, directory_to_scan, max_depth, include_hidden, min_f
                     item_type += " (Important System Folder)"
                 f.write(f"{item_type}: {item} - Size: {size_str}\n")
 
-        # Append scan results to the history log
-        with open('history.log', 'a') as history_file:
-            history_file.write(f"\nScan Results - {time.strftime('%Y-%m-%d %H:%M:%S')}\n")
-            history_file.write(f"Directory: {directory_to_scan}\n")
-            history_file.write(f"Scan Type: {scan_type}\n")
-            history_file.write(f"Number of Files Displayed: {num_files_to_display}\n")
-            history_file.write(f"Output File: {output_file}\n")
-            history_file.write(f"Resource Level: {resource_level}\n\n")
-            history_file.write("Top Files:\n")
-            for item, size, is_important in files[:num_files_to_display]:
-                size_str = format_size(size)
-                item_type = "File"
-                if is_important:
-                    item_type += " (Important System File)"
-                history_file.write(f"{item_type}: {item} - Size: {size_str}\n")
-            history_file.write("\nTop Folders:\n")
-            for item, size, is_important in folders[:num_files_to_display]:
-                size_str = format_size(size)
-                item_type = "Folder"
-                if is_important:
-                    item_type += " (Important System Folder)"
-                history_file.write(f"{item_type}: {item} - Size: {size_str}\n")
+        if history_logging:
+            with open('history.log', 'a') as history_file:
+                history_file.write(f"\nScan Results - {time.strftime('%Y-%m-%d %H:%M:%S')}\n")
+                history_file.write(f"Directory: {directory_to_scan}\n")
+                history_file.write(f"Scan Type: {scan_type}\n")
+                history_file.write(f"Number of Files Displayed: {num_files_to_display}\n")
+                history_file.write(f"Output File: {output_file}\n")
+                history_file.write(f"Resource Level: {resource_level}\n\n")
+                history_file.write("Top Files:\n")
+                for item, size, is_important in files[:num_files_to_display]:
+                    size_str = format_size(size)
+                    item_type = "File"
+                    if is_important:
+                        item_type += " (Important System File)"
+                    history_file.write(f"{item_type}: {item} - Size: {size_str}\n")
+                history_file.write("\nTop Folders:\n")
+                for item, size, is_important in folders[:num_files_to_display]:
+                    size_str = format_size(size)
+                    item_type = "Folder"
+                    if is_important:
+                        item_type += " (Important System Folder)"
+                    history_file.write(f"{item_type}: {item} - Size: {size_str}\n")
 
-    # Print the largest files and folders to the console
     print("\nTop Largest Files:")
     for item, size, is_important in files[:num_files_to_display]:
         size_str = format_size(size)
@@ -178,6 +201,7 @@ if __name__ == "__main__":
     parser.add_argument("--num-files", type=int, default=10, help="Number of top files and folders to display (default is 10)")
     parser.add_argument("--output-file", help="Output file to store the results (optional)")
     parser.add_argument("--resource-level", choices=["low", "medium", "high"], default="medium", help="Resource usage level: low, medium, high")
+    parser.add_argument("--no-history", action="store_true", help="Disable history logging")
     args = parser.parse_args()
 
     scan_type = args.scan_type
@@ -189,13 +213,16 @@ if __name__ == "__main__":
     num_files_to_display = args.num_files
     output_file = args.output_file
     resource_level = args.resource_level
+    history_logging = not args.no_history
+    pause_event = threading.Event()
 
     if scan_type == "quick":
         main_threaded("quick", directory_to_scan, max_depth, include_hidden, min_file_size, valid_extensions,
-                      num_files_to_display, output_file, resource_level)
+                      num_files_to_display, output_file, resource_level, history_logging, pause_event)
     elif scan_type == "full":
         main_threaded("full", directory_to_scan, max_depth, include_hidden, min_file_size, valid_extensions,
-                      num_files_to_display, output_file, resource_level)
+                      num_files_to_display, output_file, resource_level, history_logging, pause_event)
     elif scan_type == "custom":
         main_threaded("custom", directory_to_scan, max_depth, include_hidden, min_file_size, valid_extensions,
-                      num_files_to_display, output_file, resource_level)
+                      num_files_to_display, output_file, resource_level, history_logging, pause_event)
+
